269d9f79016855963a8107b152ff703df14a2b1f
Ivan-G0nzalez <iegdlt@gmail.com>
2022-05-28 18:11:54 -0500

    la calculadora funciona

--- main.js
+++ main.js
@@ -12,20 +12,80 @@ class Calculator {
     this.operation = undefined
   }
 
-  delete() {}
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
 
   appendNumber(number) {
-    this.currentOperand = number;
+
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
-  chooseOperation(operation){}
+  chooseOperation(operation){
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== ''){
+      this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = '' 
+
+  }
+
 
   compute(){
-
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      default:
+        return  
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
   }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let intergerDisplay
+    if (isNaN(integerDigits)) {
+      intergerDisplay = ''
+    } else {
+      intergerDisplay =  integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0})
+    }
+    if (decimalDigits != null) {
+      return `${intergerDisplay}.${decimalDigits}`
+    } else {
+      return integerDigits
+    }
+  }
+
 
   updateDisplay(){
     this.currentOperandTextElement.innerText = this.currentOperand;
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
+    }
   }
 }
 
@@ -47,6 +107,26 @@ numberButtons.forEach(button => {
   })
 })
 
-console.log(calculator);
 
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
 
